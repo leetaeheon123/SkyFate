290cfd9cb51efb285b40eac71451a867668a755b
SeonjaeHyeon <hsj106@mju.ac.kr>
2023-01-01 23:04:41 +0900

    ESLint/Prettier lint 설정 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,15 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
+  extends: [
+    '@react-native-community',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': ['error', {endOfLine: 'auto'}],
+  },
 };
-
