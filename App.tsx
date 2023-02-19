800c11cdf12a627add4b4f879d799de44d6b50cc
Seonjae Hyeon <hsj106@mju.ac.kr>
2023-02-19 13:50:22 +0900

    [Sentry] 샘플 이벤트 제거

--- App.tsx
+++ App.tsx
@@ -95,8 +95,6 @@ const App = () => {
             level: 'error',
           });
           setCurrentRouteName(state?.routes[state.index].name);
-
-          Sentry.captureEvent({message: 'Sentry Test'});
         }}>
         <AppContext.Provider value={initialState}>
           <NativeBaseProvider>
